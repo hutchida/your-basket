cda7c681ac813e323bd94e917308c7e5a809bb87
Daniel Hutchings <39595766+hutchida@users.noreply.github.com>
2021-03-20 21:11:44 +0000

    Initial commit

---
+++ src/components/Cart.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="cart">
+    <h1 class="title">Your Basket</h1>
+    <p>
+      Items you have added to your basket are shown below.<br />
+      Adjust the quantities or remove items before continuing your purchase.
+    </p>
+    <table class="table is-striped" v-show="products.length">
+      <thead>
+        <tr>
+          <td>Product</td>
+          <td>Price</td>
+          <td>Qty</td>
+          <td>Cost</td>
+          <td></td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="p in products" track-by="id" :key="p.id">
+          <template v-if="p.quantity > 0">
+            <td>{{ p.name }}</td>
+            <td>£{{ p.price }}</td>
+            <td>
+              <div class="input-group">
+                <div class="input-element">
+                  <input
+                    @change="updateQuantity(p)"
+                    size="2"
+                    id="quantity"
+                    type="number"
+                    class="form-control input-number"
+                    :value="p.quantity"
+                    min="1"
+                    max="10"
+                    onfocus="this.previousValue = this.value"
+                    onkeydown="this.previousValue = this.value"
+                    oninput="validity.valid || (value = this.previousValue)"
+                  />
+                </div>
+
+                <div class="input-element">
+                  <button
+                    @click="incrementCart(p)"
+                    type="button"
+                    class="btn btn-default btn-number"
+                    data-type="plus"
+                    data-field="quant[1]"
+                  >
+                    +
+                  </button>
+                  <button
+                    @click="decrementCart(p)"
+                    type="button"
+                    class="btn btn-default btn-number"
+                    data-type="minus"
+                    data-field="quant[1]"
+                  >
+                    -
+                  </button>
+                </div>
+              </div>
+            </td>
+            <td>£{{ round(p.quantity * p.price, 2) }}</td>
+            <td>
+              <a @click="makeQuantityZero({ product: p, quantity: 0 })" href="#"
+                ><img src="../assets/trash.svg" alt="Remove"
+              /></a>
+            </td>
+          </template>
+        </tr>
+
+        <tr></tr>
+        <tr>
+          <td>Subtotal:</td>
+          <td></td>
+          <td></td>
+          <td>£{{ total }}</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>VAT @ 20%:</td>
+          <td></td>
+          <td></td>
+          <td>£{{ round(total * 0.2, 2) }}</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td><b>Total Cost</b></td>
+          <td></td>
+          <td></td>
+          <td>
+            <b>£{{ total }}</b>
+          </td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+    <p>
+      <button
+        v-show="products.length"
+        class="button is-primary"
+        @click="checkout"
+      >
+        Buy Now >>
+      </button>
+    </p>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters({
+      products: "cartProducts",
+    }),
+    total() {
+      return this.products.reduce((total, p) => {
+        return total + p.price * p.quantity;
+      }, 0);
+    },
+  },
+  methods: {
+    checkout() {
+      alert("Pay us £" + this.total);
+    },
+    round(value, decimals) {
+      return Number(
+        Math.round(value + "e" + decimals) + "e-" + decimals
+      ).toFixed(decimals);
+    },
+
+    ...mapActions([
+      "incrementCart",
+      "decrementCart",
+      "makeQuantityZero",
+      "updateQuantity",
+    ]),
+  },
+};
+</script>
+
+<style>
+.input-element {
+  float: left;
+}
+
+.input-number {
+  height: 30px;
+  text-align: right;
+}
+.input-group button {
+  display: block;
+  width: 25px;
+  height: 18px;
+}
+
+
+input#quantity {
+  text-align: right;
+  border: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+  margin: 0px 5px 0px 0px;
+  width: 40px;
+  height: 32px;
+}
+
+input[type=number]::-webkit-inner-spin-button,
+input[type=number]::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,156 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as types from './mutation-types'
+
+Vue.use(Vuex)
+
+const debug = process.env.NODE_ENV !== 'production'
+
+// initial state
+const state = {
+    added: [
+        {
+            id: 'cc919e21-ae5b-5e1f-d023-c40ee669520c',
+            name: 'Cotton T-Shirt, Medium',
+            description: '',
+            price: 1.99,
+            quantity: 1
+
+        },
+        {
+            id: 'bcd755a6-9a19-94e1-0a5d-426c0303454f',
+            name: 'Baseball Cap, One Size',
+            description: '',
+            price: 2.99,
+            quantity: 2
+        },
+        {
+            id: '727026b7-7f2f-c5a0-ace9-cc227e686b8e',
+            name: 'Swim Shorts, Medium',
+            description: '',
+            price: 3.99,
+            quantity: 1
+        }
+    ],
+    all: [
+        {
+            id: 'cc919e21-ae5b-5e1f-d023-c40ee669520c',
+            name: 'Cotton T-Shirt, Medium',
+            description: '',
+            price: 1.99,
+            quantity: 1
+
+        },
+        {
+            id: 'bcd755a6-9a19-94e1-0a5d-426c0303454f',
+            name: 'Baseball Cap, One Size',
+            description: '',
+            price: 2.99,
+            quantity: 2
+        },
+        {
+            id: '727026b7-7f2f-c5a0-ace9-cc227e686b8e',
+            name: 'Swim Shorts, Medium',
+            description: '',
+            price: 3.99,
+            quantity: 1
+        }
+    ]
+}
+
+// getters
+const getters = {
+    allProducts: state => state.all, // would need action/mutation if data fetched async
+    getNumberOfProducts: state => (state.all) ? state.all.length : 0,
+    cartProducts: state => {
+        return state.added.map(({ id, quantity }) => {
+            const product = state.all.find(p => p.id === id)
+
+            return {
+                name: product.name,
+                price: product.price,
+                id: product.id,
+                quantity
+            }
+        })
+    }
+}
+
+// actions
+const actions = {
+    updateQuantity({ commit }, product) {
+        commit(types.UPDATE_QUANTITY, {
+            id: product.id,
+            q: event.target.value
+        })
+    },
+
+    incrementCart({ commit }, product) {
+        commit(types.INCREMENT_CART, {
+            id: product.id
+        })
+    },
+
+    decrementCart({ commit }, product) {
+        commit(types.DECREMENT_CART, {
+            id: product.id
+        })
+    },
+
+    makeQuantityZero({ commit }, { product, quantity }) {
+        commit(types.MAKE_QUANTITY_ZERO, {
+            id: product.id,
+            q: quantity
+        })
+    }
+}
+
+// mutations
+const mutations = {
+
+    [types.UPDATE_QUANTITY](state, { id, q }) {
+        const record = state.added.find(p => p.id === id)
+        if (record) {
+            record.quantity = q
+        }
+    },
+
+    [types.INCREMENT_CART](state, { id }) {
+        const record = state.added.find(p => p.id === id)
+        if (!record) {
+            state.added.push({
+                id,
+                quantity: 1
+            })
+        } else {
+            if (record.quantity < 10) {
+                record.quantity++
+            }
+
+        }
+    },
+
+    [types.DECREMENT_CART](state, { id }) {
+        const record = state.added.find(p => p.id === id)
+
+        if (record.quantity > 1) {
+            record.quantity--
+        }
+    },
+
+    [types.MAKE_QUANTITY_ZERO](state, { id, q }) {
+        const record = state.added.find(p => p.id === id)
+        if (record) {
+            record.quantity = q
+        }
+    }
+}
+
+// one store for entire application
+export default new Vuex.Store({
+    state,
+    strict: debug,
+    getters,
+    actions,
+    mutations
+})
